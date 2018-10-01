8fe4834b0ed3d2296624985b6daecac479f9fb6e
Veronica L <verolozada12@gmail.com>
2018-10-01 11:56:45 -0400

    href for BonVoyage in the header modified

--- home.js
+++ home.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     let country;
 
     $("#test").on("click", function () {
+        // event.preventDefault();
         console.log("I've been clicked");
         city = $("#autocomplete-input1").val().trim();
         console.log(city);
@@ -11,3 +12,4 @@ $(document).ready(function () {
         console.log(country);
     });
 });
+
