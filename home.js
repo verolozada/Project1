a7945e28a6f1c2731e837e9764cdf1f7e4111471
Bradley <brad.driver33@gmail.com>
2018-10-04 12:38:27 -0400

    removed the user.html link from the submit button on the home page because I couldn't bring up the console log.

--- home.js
+++ home.js
@@ -10,21 +10,31 @@ $(document).ready(function () {
         console.log(city);
         country = $("#autocomplete-input2").val().trim();
         console.log(country);
-<<<<<<< HEAD
         // Attempting to call fourSquare API
-         var type = $(this).attr("name");
         var fsURl = 'https://api.foursquare.com/v2/venues/explore?client_id=D4PJXJ4YDXIGGJXL0HU1XM1AZFXHDZTXFCIY0XKP5ALGUXJL&client_secret=L5O4FMUQAILJDX4SYQJBJLGXMG31P5LIGN2OQVL3WC4JVIU5&v=20180323&limit=4&near=' + city + ',' + country;
         console.log(fsURl)
         $.ajax({
             url: fsURl,
             method: "GET"
-        }).then(function (response){
+        }).then(function (response) {
             console.log('Response from FourSquare===>', response);
+            const { response:{ groups }} = response
+            const venues = []
+            groups[0].items.forEach(item => venues.push(item.venue.name))
+            console.log('normalized DATA ==>', venues)
+            // Looping through each result item.
+            // for (var f = 0; f < data.length; f++) {
+            //     // Creating and storing the thingsToDo div as the varaible recommendedDiv
+            //     var recommendedDiv = $("#thingsToDo");
+                    venues.forEach(venue => $("#venueName").append(venue))
+            //     //Creating a paragraph tag with the responses venue name in it.
+            //     var r = $("<p>").text(data[f].venue.name);
+
+            // }
         })
-=======
-        console.log("HEllo");
->>>>>>> 26b3546a930e68c9932e947b920ba350eb491392
     });
+
+
 
 
 
