9725fbf12cfd26093df4e0031fb7f5ac28b552a3
bshephe9 <brookeshepherd672@gmail.com>
2018-10-04 20:03:49 -0400

    edits to reviews

--- home.js
+++ home.js
@@ -1,54 +1,7 @@
 $(document).ready(function () {
 
-<<<<<<< HEAD
-//Add in enter button functionality for submit button
-    var input = document.getElementById("test"); 
 
-    // Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Number 13 is the "Enter" key on the keyboard
-    if (event.keyCode === 13) {
-      // Trigger the button element with a click
-      document.getElementById("test").click();
-    }
-  });
+   
 
 
-  let city;
-  let country;
-=======
-    let city;
-    let country;
-
-    $("#test").on("click", function () {
-        // event.preventDefault();
-        console.log("I've been clicked");
-        city = $("#autocomplete-input1").val().trim();
-        console.log(city);
-        country = $("#autocomplete-input2").val().trim();
-        console.log(country);
-<<<<<<< HEAD
-        // Attempting to call fourSquare API
-         var type = $(this).attr("name");
-        var fsURl = 'https://api.foursquare.com/v2/venues/explore?client_id=D4PJXJ4YDXIGGJXL0HU1XM1AZFXHDZTXFCIY0XKP5ALGUXJL&client_secret=L5O4FMUQAILJDX4SYQJBJLGXMG31P5LIGN2OQVL3WC4JVIU5&v=20180323&limit=4&near=' + city + ',' + country;
-        console.log(fsURl)
-        $.ajax({
-            url: fsURl,
-            method: "GET"
-        }).then(function (response){
-            console.log('Response from FourSquare===>', response);
-        })
-=======
-        console.log("HEllo");
->>>>>>> 26b3546a930e68c9932e947b920ba350eb491392
-    });
-
-
-// Translate API (Yandex)
->>>>>>> 1fc3d6c93361c6623d43a58aaae206317e03d102
-
-
-});
 
