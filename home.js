8e708f2acc5908d19641ac888cd34a29a71a7899
bshephe9 <brookeshepherd672@gmail.com>
2018-10-06 17:10:23 -0400

    commit message

--- home.js
+++ home.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
-
-    var GoogleAuth;
+   
 
     $("#test").on("click", function () {
         // event.preventDefault();
@@ -28,39 +27,56 @@ $(document).ready(function () {
             });
 
         })
-    });
 
-    function search(){ 
-        //Clear results
-        $('.vlogs').html();
-        $('#test').html();
+      
+//Insert YouTube API Call 
+function youtubeApiCall() {
+            $.ajax({
+                cache: false,
+                data: $.extend({
+                    key: 'AIzaSyDnJZQENCNACGclR0grH3IoXQJNGEZHEtM',
+                    q: $('#hyv-search').val(),
+                    part: 'snippet'
+                }, { maxResults: 5, pageToken: $("#pageToken").val() }),
+                dataType: 'json',
+                type: 'GET',
+                timeout: 5000,
+                url: 'https://www.googleapis.com/youtube/v3/search'
+            })
+                .done(function (data) {
+                    $('.btn-group').show();
+                    if (typeof data.prevPageToken === "undefined") {
+                        $("#pageTokenPrev").hide();
+                    } else {
+                        $("#pageTokenPrev").show();
+                    }
+                    if (typeof data.nextPageToken === "undefined") {
+                        $("#pageTokenNext").hide();
+                    } else {
+                        $("#pageTokenNext").show();
+                    }
+                    var items = data.items, videoList = "";
+                    $("#pageTokenNext").val(data.nextPageToken);
+                    $("#pageTokenPrev").val(data.prevPageToken);
+                    $.each(items, function (index, e) {
+                        videoList = videoList + '<li class="hyv-video-list-item"><div class="hyv-content-wrapper"><a href="" class="hyv-content-link" title="' + e.snippet.title + '"><span class="title">' + e.snippet.title + '</span><span class="stat attribution">by <span>' + e.snippet.channelTitle + '</span></span></a></div><div class="hyv-thumb-wrapper"><a href="" class="hyv-thumb-link"><span class="hyv-simple-thumb-wrap"><img alt="' + e.snippet.title + '" src="' + e.snippet.thumbnails.default.url + '" width="120" height="90"></span></a></div></li>';
+                    });
+                    $(".test").html(videoList);
+                    // JSON Responce to display for user
+                    new PrettyJSON.view.Node({
+                        el: $(".hyv-watch-sidebar-body"),
+                        data: data
+                    });
+                });
+        }
 
-        //Get Form Input 
-        q = $ ("#query").val();
-
-        //Run GET request on the API 
-        $.get(
-            "https://www.googleapis.com/youtube/v3/search",{
-            part: 'snippet, id',
-            q: q, 
-            type: 'video',
-            key: AIzaSyDnJZQENCNACGclR0grH3IoXQJNGEZHEtM},
-            function(data){
-                var nextPageToken = data.nextPageToken;
-                var prevPageToken = data.prevPageToken;
-             }
-        );
-
-    }
-
-
-
+    })
 
 
 
 });
 
-    
+
 
 
 
