811273dbbe571e9876c06e7dfa9034497adf3142
bshephe9 <brookeshepherd672@gmail.com>
2018-10-06 17:13:01 -0400

    commit

--- home.js
+++ home.js
@@ -28,52 +28,6 @@ $(document).ready(function () {
 
         })
 
-      
-//Insert YouTube API Call 
-function youtubeApiCall() {
-            $.ajax({
-                cache: false,
-                data: $.extend({
-                    key: 'AIzaSyDnJZQENCNACGclR0grH3IoXQJNGEZHEtM',
-                    q: $('#hyv-search').val(),
-                    part: 'snippet'
-                }, { maxResults: 5, pageToken: $("#pageToken").val() }),
-                dataType: 'json',
-                type: 'GET',
-                timeout: 5000,
-                url: 'https://www.googleapis.com/youtube/v3/search'
-            })
-                .done(function (data) {
-                    $('.btn-group').show();
-                    if (typeof data.prevPageToken === "undefined") {
-                        $("#pageTokenPrev").hide();
-                    } else {
-                        $("#pageTokenPrev").show();
-                    }
-                    if (typeof data.nextPageToken === "undefined") {
-                        $("#pageTokenNext").hide();
-                    } else {
-                        $("#pageTokenNext").show();
-                    }
-                    var items = data.items, videoList = "";
-                    $("#pageTokenNext").val(data.nextPageToken);
-                    $("#pageTokenPrev").val(data.prevPageToken);
-                    $.each(items, function (index, e) {
-                        videoList = videoList + '<li class="hyv-video-list-item"><div class="hyv-content-wrapper"><a href="" class="hyv-content-link" title="' + e.snippet.title + '"><span class="title">' + e.snippet.title + '</span><span class="stat attribution">by <span>' + e.snippet.channelTitle + '</span></span></a></div><div class="hyv-thumb-wrapper"><a href="" class="hyv-thumb-link"><span class="hyv-simple-thumb-wrap"><img alt="' + e.snippet.title + '" src="' + e.snippet.thumbnails.default.url + '" width="120" height="90"></span></a></div></li>';
-                    });
-                    $(".test").html(videoList);
-                    // JSON Responce to display for user
-                    new PrettyJSON.view.Node({
-                        el: $(".hyv-watch-sidebar-body"),
-                        data: data
-                    });
-                });
-        }
-
-    })
-
-
-
 });
 
 
