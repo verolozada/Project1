868486edbf69af3d14ce33a57616d6f3519891f5
bshephe9 <brookeshepherd672@gmail.com>
2018-10-09 11:44:55 -0400

    added in youtube js but still not functioning

--- home.js
+++ home.js
@@ -1,5 +1,4 @@
 $(document).ready(function () {
-   
 
     $("#test").on("click", function () {
         // event.preventDefault();
@@ -21,9 +20,9 @@ $(document).ready(function () {
             // const venuesAddress = [];
             groups[0].items.forEach(item => venues.push(item.venue.name + " Category: " + (item.venue.categories[0].name) + " Address:  " + (item.venue.location.address)));
             console.log('normalized DATA ==>', venues);
-             venues.forEach(venue => {
-             console.log(venue);
-             $("#thingsToDo").append('<p>' + venue.name + '</p>');
+            venues.forEach(venue => {
+                console.log(venue);
+                $("#thingsToDo").append('<p>' + venue.name + '</p>');
             });
 
             venues.forEach(venue => {
@@ -31,10 +30,46 @@ $(document).ready(function () {
                 $(".thingToDo").append('<p>' + venue + '</p>');
             });
 
-        })
-
+        });
     });
 
+    function tplawesome(e, t) { res = e; for (var n = 0; n < t.length; n++) { res = res.replace(/\{\{(.*?)\}\}/g, function (e, r) { return t[n][r] }) } return res }
+
+
+    function googleApiClientReady() {
+        gapi.client.setApiKey("AIzaSyD3VXMd8bzXIQijinIuKOKsgqQ1mg6bv80");
+        gapi.client.load('youtube', 'v3', function () {
+            searchA();
+        });
+    }
+    function searchA() {
+        var q = encodeURIComponent($("#search").val()).replace(/%20/g, "+")
+        var request = gapi.client.youtube.channels.list({
+            part: 'snippet',
+            maxResults: 3,
+            order: "viewCount",
+            publishedAfter: "2017-01-01T00:00:00Z",
+        });
+        //execute request 
+        request.execute(function (response) {
+            var results = response.result;
+            $(".vlogs").html("");
+            $.each(results.items, function (index, item) {
+                $.get("tpl/item.html", function (data) {
+                    $(".vlogs").append(tplawesome(data, [{ "title": item.snippet.title, "videoid": item.id.videoID }]));
+                });
+            });
+            resetVideoHeight();
+        });
+    };
+
+$(window).on("resize", resetVideoHeight);
+
+function resetVideoHeight() {
+    $("video").css("height")
+} 
+
+});
 
 
 
